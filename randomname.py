89f749221e054604a57484b6c0e1a6f2489f4be6
Mike T. <124411771+Thebodytalk@users.noreply.github.com>
2025-05-28 18:24:38 -0400

    Add script to randomly generate name from .txt

--- randomname.py
+++ randomname.py
@@ -1,5 +1,26 @@
 import random
 
-with open("./practice.txt", "r") as file:
-    for line in file:
-        print(line.strip())
+# with open("./practice.txt", "r") as file:
+    # for line in file:
+        # print(line.strip())
+        # test = [line.strip()]
+
+    # print('list')
+    # print(test[0])
+    # print(test[1])
+    # print(test[2])
+
+first = []
+last = []
+
+with open("./firstname.txt", "r") as fileFirst:
+    for line in fileFirst:
+        first.append(line.strip())
+        
+    print(random.choice(first))
+
+with open("./lastname.txt", "r") as fileLast:
+    for line in fileLast:
+        last.append(line.strip())
+
+    print(random.choice(last))
